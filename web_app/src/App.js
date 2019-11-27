1e9496fe23bbbf01eae8f968a4f2f70e0c072a63
DomGarguilo <dominic.garguilo@gmail.com>
2019-11-27 15:02:07 -0500

    changes app into component

--- web_app/src/App.js
+++ web_app/src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,7 +21,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
