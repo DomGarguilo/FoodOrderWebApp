a9a147e8da77389a45d1f0fc2ccd9db27179e661
mrana1 <32280437+mrana1@users.noreply.github.com>
2019-11-27 16:19:45 -0500

    login and selection jsx

---
+++ web_app/src/pages/login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MainPage = () => {
+
+    return (
+        <div>
+            <h3>react router test</h3>
+            <small>Main page</small>
+        </div>
+    )
+
+}
+
+export default MainPage;
