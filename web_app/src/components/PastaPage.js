835e0ad0647f666b8b57e684937f1cd719951fe8
springroII <48109235+springroII@users.noreply.github.com>
2019-11-29 19:10:26 -0500

    selection page + pasta page labels

--- web_app/src/components/PastaPage.js
+++ web_app/src/components/PastaPage.js
@@ -13,14 +13,12 @@ class PastaPage extends Component {
 
           {/*WE NEED TO MAKE IT SO PASTA TYPE IS REQUIRED AND TELLS USER ON SUBMIT*/}
           <form name="pasta_form" method="GET" action="review_order.html" class="order">
-            <input type="checkbox" id="c_pasta" name="PastaItem" onclick="displayPastaOptions(checked)" class="radio-tools" />
             <label for="c_pasta" class="item_label">Select Pasta Type</label><br /><br />
             <div id="pasta_options">
               <input type="radio" id="penne" name="pasta_type" value="penne" class="all_options" required /> <label for="penne" class="option_label">Penne</label>
               <input type="radio" id="bowtie" name="pasta_type" value="bowtie" class="all_options" required /> <label for="bowtie" class="option_label">Bowtie</label>
             </div> {/*end pasta options*/} <br />
 
-            <input type="checkbox" id="c_sauce" name="PastaItem" onclick="displaySauceOptions(checked)" class="radio-tools" />
             <label for="c_sauce" class="item_label">Select Sauce Type</label><br /><br />
             <div id="sauce_options">
               <input type="checkbox" id="pesto" name="sauce_type" value="pesto" class="all_options" /> <label for="pesto" class="option_label">Pesto</label><br />
@@ -28,7 +26,6 @@ class PastaPage extends Component {
               <input type="checkbox" id="alfredo" name="sauce_type" value="alfredo" class="all_options" /> <label for="alfredo" class="option_label">Alfredo</label><br />
             </div> {/*end sauce options*/} <br />
 
-            <input type="checkbox" id="c_protein" name="PastaItem" onclick="displayProteinOptions(checked)" class="radio-tools" />
             <label for="c_protein" class="item_label">Select Proteins</label><br /><br />
             <div id="protein_options">
               <input type="checkbox" id="chicken" name="protein" value="chicken" class="all_options" /> <label for="chicken" class="option_label">Chicken</label><br />
@@ -38,7 +35,6 @@ class PastaPage extends Component {
               <input type="checkbox" id="crab_meat" name="protein" value="crabmeat" class="all_options" /> <label for="crab_meat" class="option_label">Crab Meat</label><br />
             </div> {/*end sauce options*/} <br />
 
-            <input type="checkbox" id="c_topping" name="PastaItem" value="pancakes" onclick="displayToppingOptions(checked)" class="radio-tools" />
             <label for="c_topping" class="item_label">Select Additional Toppings</label><br /><br />
             <div id="topping_options">
               <input type="checkbox" id="onion" name="ingredients" value="onion" class="all_options" /> <label for="onion" class="option_label">Onion</label><br />
@@ -48,7 +44,6 @@ class PastaPage extends Component {
               <input type="checkbox" id="corn" name="ingredients" value="Corn" class="all_options" /> <label for="corn" class="option_label">Corn</label><br />
             </div> {/*end toppings options*/} <br />
 
-            <input type="checkbox" id="c_seasoning" name="PastaItem" value="pancakes" onclick="displaySeasoningOptions(checked)" class="radio-tools" />
             <label for="c_seasoning" class="item_label">Select Seasonings</label><br /><br />
             <div id="seasoning_options">
               <input type="checkbox" id="saltNpep" name="seasonings" value="saltAndPepper" class="all_options" /> <label for="saltNpep" class="option_label">Salt and Pepper</label><br />
--- web_app/src/components/SelectionPage.js
+++ web_app/src/components/SelectionPage.js
@@ -1,37 +1,75 @@
+// import React, { Component } from 'react';
+// import { Link } from "react-router-dom";
+// import "../styles/selectionStyle.css";
+// import "../styles/commonStyle.css"
+
+// class SelectionPage extends Component {
+//   render() {
+//     return (
+//       <body>
+//         <div className="selectionPage" id="container">
+//           <img id="login_bg" />
+//           <h1 id="head">Make a Selection</h1>
+
+//           <input type='hidden' value='' name='studentID' id='studentID' />
+//           <div class='order'>
+//             <div id='t1'>
+
+//               <figure id='breakfast'>
+//                 <figcaption id="text">BREAKFAST
+//                         </figcaption>
+//                 <img id="bf" />
+//               </figure>
+//             </div>
+//             <div id='t2'>
+
+//               <figure id='pasta'>
+//                 <figcaption >PASTA</figcaption>
+//                 <img id="p" />
+
+//               </figure>
+//             </div>
+//           </div>
+//         </div>
+//       </body>
+//     );
+//   }
+// }
+
+// export default SelectionPage;
+
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import "../styles/selectionStyle.css";
-import "../styles/commonStyle.css"
+import "../styles/breakfastStyle.css";
+import "../styles/commonStyle.css";
 
 class SelectionPage extends Component {
   render() {
     return (
       <body>
-        <div className="selectionPage" id="container">
+        <div className="SelectionPage" id="container">
           <img id="login_bg" />
-          <h1 id="head">Make a Selection</h1>
 
-          <input type='hidden' value='' name='studentID' id='studentID' />
-          <div class='order'>
-            <div id='t1'>
+          <h1 id="head">Menu</h1>
+          <h3 id="please_select">Please Select an Item:</h3> <br />
 
-              <figure id='breakfast'>
-                <figcaption id="text">BREAKFAST
-                        </figcaption>
-                <img id="bf" />
-              </figure>
-            </div>
-            <div id='t2'>
+          <form action="review_order.html" method="GET" name="MenuItem" class="order">
 
-              <figure id='pasta'>
-                <figcaption >PASTA</figcaption>
-                <img id="p" />
+            <Link to = "/breakfast">
+            <input type="radio" id="breakfast" name="BFItem" value="breakfast" class="radio-tools" />
+            </Link>
+            <label for="breakfast" class="item_label">Breakfast</label> <br /><br />
 
-              </figure>
-            </div>
-          </div>
+            <Link to = "/pasta">
+            <input type="radio" id="pasta" name="PItem" value="pasta" class="radio-tools" />
+            </Link>
+            <label for="pasta" class="item_label">Pasta</label> <br /><br />
+            
+          </form>
+
         </div>
       </body>
+
     );
   }
 }
