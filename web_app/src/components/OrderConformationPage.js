386c82d128fe66554c45c5c9dce317f74bb63b31
mfranz1 <55514493+mfranz1@users.noreply.github.com>
2019-11-30 16:15:52 -0500

    Update OrderConformationPage.js

--- web_app/src/components/OrderConformationPage.js
+++ web_app/src/components/OrderConformationPage.js
@@ -6,9 +6,16 @@ import "../styles/commonStyle.css"
 class OrderConformationPage extends Component {
   render() {
     return (
-    <div className="OrderConformationPage" id="container">
-        Here is where the order confirmation page will be.
-    </div>
+      <div className="OrderConformationPage" id="container">
+        {/*Used to pass studentID from previous webpage*/}
+        <input type='hidden' value='' name='studentID' id='studentID' />
+        <img id="login_bg" />
+        <h1 id="head">Review Order</h1><br/>
+        <ol>
+          <li></li>
+        </ol>
+        <h3 id="please_select">Please Select an Option:</h3> <br />
+      </div>
     );
   }
 }
