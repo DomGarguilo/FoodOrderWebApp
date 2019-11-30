494d9da34349bd1b33a5c079445d8f5adc17a427
Thomas679 <49179886+Thomas679@users.noreply.github.com>
2019-11-30 09:53:43 -0500

    AddedIMages
    Stuff

--- web_app/src/components/BaconOptions.js
+++ web_app/src/components/BaconOptions.js
@@ -17,10 +17,10 @@ class BaconOptions extends Component {
                 <form method="GET" name="BreakfastItem" class="order">
 
                 <div id="bacon_options">
-                    <input type="radio" name="smoked" id="smoked" value="smoked" class="all_options" /> <label for="smoked" class="option_label">Smoked</label><br />
-                    <input type="radio" name="turkey" id="turkey" value="turkey" class="all_options" /> <label for="turkey" class="option_label">Turkey Bacon</label><br />
-                    <input type="radio" name="canadian" id="canadian" value="canadian" class="all_options" /> <label for="canadian" class="option_label">Canadian Bacon</label><br />
-                    <input type="radio" name="pancetta" id="pancetta" value="pancetta" class="all_options" /> <label for="pancetta" class="option_label">Pancetta</label><br />
+                    <input type="radio" name="smoked" id="smoked" value="smoked" class="all_options" /> <label id = 'l_smoked'for="smoked" class="option_label"></label><p id='text'>Smoked</p><br />
+                    <input type="radio" name="turkey" id="turkey" value="turkey" class="all_options" /> <label id='l_turkey'for="turkey" class="option_label"></label><p id='text'>Turkey Bacon</p><br />
+                    <input type="radio" name="canadian" id="canadian" value="canadian" class="all_options" /> <label id= 'l_canadian' for="canadian" class="option_label"></label><p id='text'>Canadian Bacon</p><br />
+                    <input type="radio" name="pancetta" id="pancetta" value="pancetta" class="all_options" /> <label id = 'l_pancetta' for="pancetta" class="option_label"></label><p id='text'>Pancetta</p><br />
                  </div> <br />
 
                 <Link to="/orderConfirmation"><input type="submit" id="button" value="Add item to order" /></Link>
--- web_app/src/components/PancakeOptions.js
+++ web_app/src/components/PancakeOptions.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import "../styles/breakfastStyle.css";
 import "../styles/commonStyle.css";
-
 class PancakeOptions extends Component {
 
     render() {
@@ -25,10 +24,10 @@ class PancakeOptions extends Component {
                         </h4>
                         <br></br>
                         <br></br>
-                        <input type="checkbox" name="sugar" id="sugar" class="all_options" /> <label for="sugar" class="option_label">Sugar</label><br />
-                        <input type="checkbox" name="syrup" id="syrup" class="all_options" /> <label for="syrup" class="option_label">Syrup</label><br />
-                        <input type="checkbox" name="berries" id="berries" class="all_options" /> <label for="berries" class="option_label">Berries</label><br />
-                        <input type="checkbox" name="wh_cream" id="wh_cream" class="all_options" /> <label for="wh_cream" class="option_label">Whipped Cream</label><br />
+                        <input type="checkbox" name="sugar" id="sugar" class="all_options" /> <label id='l_sugar' for="sugar" class="option_label"></label><p id='text'>Sugar</p><br />
+                        <input type="checkbox" name="syrup" id="syrup" class="all_options" /> <label id = 'l_syrup'for="syrup" class="option_label">Syrup</label><p id='text'>Syrup</p><br />
+                        <input type="checkbox" name="berries" id="berries" class="all_options" /> <label id='l_berries' for="berries" class="option_label"></label><p id='text'>Berries</p><br />
+                        <input type="checkbox" name="wh_cream" id="wh_cream" class="all_options" /> <label id='l_wh_cream' for="wh_cream" class="option_label"></label><p id='text'>Whipped Cream</p><br />
                     </div> <br />
 
                     <Link to="/orderConfirmation"><input type="submit" id="button" value="Add item to order" /></Link>
