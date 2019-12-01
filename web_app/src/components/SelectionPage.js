fa9a447e51c8df8254ea7c4323cc51915ec384a3
Thomas679 <49179886+Thomas679@users.noreply.github.com>
2019-12-01 15:34:41 -0500

    Selection
    For Style

--- web_app/src/components/SelectionPage.js
+++ web_app/src/components/SelectionPage.js
@@ -54,17 +54,23 @@ class SelectionPage extends Component {
           <h3 id="please_select">Please Select an Item:</h3> <br />
 
           <form action="review_order.html" method="GET" name="MenuItem" class="order">
-
-            <Link to = "/breakfast">
-            <input type="radio" id="breakfast" name="BFItem" value="breakfast" class="radio-tools" />
-            </Link>
-            <label id='l_breakfast' for="breakfast" class="item_label"></label><p id='text'>Breakfast</p> <br /><br />
-
-            <Link to = "/pasta">
-            <input type="radio" id="pasta" name="PItem" value="pasta" class="radio-tools" />
-            </Link>
-            <label id='l_pasta' for="pasta" class="item_label"></label><p id='text'>Pasta</p> <br /><br />
-            
+            <div class='grid-container'>
+              <div class='grid-item'>
+                <Link to = "/breakfast">
+                <input type="radio" id="breakfast" name="BFItem" value="breakfast" class="radio-tools" />
+                </Link>
+                <label id='l_breakfast' for="breakfast" class="item_label"></label>
+                <p id='text'>Breakfast</p> 
+              </div>
+              <div class='grid-item'>
+                <Link to = "/pasta">
+                <input type="radio" id="pasta" name="PItem" value="pasta" class="radio-tools" />
+                </Link>
+                <label id='l_pasta' for="pasta" class="item_label"></label>
+                <p id='text'>Pasta</p> 
+              </div>  
+            </div>
+           
           </form>
 
         </div>
