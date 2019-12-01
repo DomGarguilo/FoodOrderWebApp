8e2319175da9f80e2cae794332d0adbee0e7c2ea
Thomas679 <49179886+Thomas679@users.noreply.github.com>
2019-12-01 15:59:05 -0500

    Gridpt2
    Grid Stuff

--- web_app/src/components/BreakfastPage.js
+++ web_app/src/components/BreakfastPage.js
@@ -21,33 +21,30 @@ class BreakfastPage extends Component {
 
             {/*Used to pass studentID from previous webpage*/}
             <input type='hidden' value='' name='studentID' id='studentID' />
-
-            <Link to = "/pancake">
-            <input type="radio" id="r_pancakes" name="BFItem" value="pancakes" onclick="displayPancakeOptions(checked)" class="radio-tools" />
-            </Link>
-            <label id='pancake_label' for="r_pancakes" class="item_label">
-              </label>
-              <p id='text'>Pancakes</p>
-              <br/><br/>
-            {/* <img id = 'img_pancake' src={im1}/> /* No Longer Needed */}
-            
-
-            <Link to = "/eggs">
-            <input type="radio" id="r_eggs" name="BFItem" value="fried_eggs" onclick="displayEggOptions(checked)" class="radio-tools" />
-            </Link>
-            <label id = 'eggs_label' for="r_eggs" class="item_label"></label>
-            <p id='text'>Fried Eggs</p>
-            <br /><br />
-            {/*<img id = 'img_eggs_fried' src={FE} >*/}
-
-            <Link to = "/bacon">
-            <input type="radio" id="r_bacon" name="BFItem" value="bacon" onclick="displayBaconOptions(checked)" class="radio-tools" />
-            </Link>
-            <label id='bacon_label' for="r_bacon" class="item_label"></label> 
-            <p id='text'>Bacon</p>
-            <br /><br />
-              {/*<img id = 'img_bacon' src={B1} />  */}
-
+            <div class='grid-container2'>
+              <div class='grid-item2'>
+                <Link to = "/pancake">
+                <input type="radio" id="r_pancakes" name="BFItem" value="pancakes" onclick="displayPancakeOptions(checked)" class="radio-tools" />
+                </Link>
+                <label id='pancake_label' for="r_pancakes" class="item_label2">
+                  </label>
+                  <p id='text'>Pancakes</p>
+              </div>
+              <div class='grid-item2'>
+                <Link to = "/eggs">
+                <input type="radio" id="r_eggs" name="BFItem" value="fried_eggs" onclick="displayEggOptions(checked)" class="radio-tools" />
+                </Link>
+                <label id = 'eggs_label' for="r_eggs" class="item_label2"></label>
+                <p id='text'>Fried Eggs</p>
+              </div>
+              <div class='grid-item2'>
+                <Link to = "/bacon">
+                <input type="radio" id="r_bacon" name="BFItem" value="bacon" onclick="displayBaconOptions(checked)" class="radio-tools" />
+                </Link>
+                <label id='bacon_label' for="r_bacon" class="item_label2"></label> 
+                <p id='text'>Bacon</p>
+              </div>
+            </div>
           </form>
 
         </div>
