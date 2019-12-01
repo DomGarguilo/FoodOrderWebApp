07c9479d3d1eda2dd8289a1507aa9e7da9dfc339
DomGarguilo <dominic.garguilo@gmail.com>
2019-12-01 16:12:37 -0500

    format and emp dashboard

--- web_app/src/components/SelectionPage.js
+++ web_app/src/components/SelectionPage.js
@@ -56,21 +56,21 @@ class SelectionPage extends Component {
           <form action="review_order.html" method="GET" name="MenuItem" class="order">
             <div class='grid-container'>
               <div class='grid-item'>
-                <Link to = "/breakfast">
-                <input type="radio" id="breakfast" name="BFItem" value="breakfast" class="radio-tools" />
+                <Link to="/breakfast">
+                  <input type="radio" id="breakfast" name="BFItem" value="breakfast" class="radio-tools" />
                 </Link>
                 <label id='l_breakfast' for="breakfast" class="item_label"></label>
-                <p id='text'>Breakfast</p> 
+                <p id='text'>Breakfast</p>
               </div>
               <div class='grid-item'>
-                <Link to = "/pasta">
-                <input type="radio" id="pasta" name="PItem" value="pasta" class="radio-tools" />
+                <Link to="/pasta">
+                  <input type="radio" id="pasta" name="PItem" value="pasta" class="radio-tools" />
                 </Link>
                 <label id='l_pasta' for="pasta" class="item_label"></label>
-                <p id='text'>Pasta</p> 
-              </div>  
+                <p id='text'>Pasta</p>
+              </div>
             </div>
-           
+
           </form>
 
         </div>
