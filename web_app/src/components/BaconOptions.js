3b12c61a0988bfb761a1e215dcdebd0a666d8367
mfranz1 <55514493+mfranz1@users.noreply.github.com>
2019-12-01 01:02:47 -0500

    Update BaconOptions.js

--- web_app/src/components/BaconOptions.js
+++ web_app/src/components/BaconOptions.js
@@ -4,7 +4,15 @@ import "../styles/breakfastStyle.css";
 import "../styles/commonStyle.css";
 
 class BaconOptions extends Component {
-
+    constructor() {
+        super();
+        this.state = {
+          smoked: 0,
+          turkey: 0,
+          canadian: 0,
+          pancetta: 0
+        }
+    }
     render() {
         return (
             <body>
@@ -17,12 +25,17 @@ class BaconOptions extends Component {
                 <form method="GET" name="BreakfastItem" class="order">
 
                 <div id="bacon_options">
-                    <input type="radio" name="smoked" id="smoked" value="smoked" class="all_options" /> <label id = 'l_smoked'for="smoked" class="option_label"></label><p id='text'>Smoked</p><br />
-                    <input type="radio" name="turkey" id="turkey" value="turkey" class="all_options" /> <label id='l_turkey'for="turkey" class="option_label"></label><p id='text'>Turkey Bacon</p><br />
-                    <input type="radio" name="canadian" id="canadian" value="canadian" class="all_options" /> <label id= 'l_canadian' for="canadian" class="option_label"></label><p id='text'>Canadian Bacon</p><br />
-                    <input type="radio" name="pancetta" id="pancetta" value="pancetta" class="all_options" /> <label id = 'l_pancetta' for="pancetta" class="option_label"></label><p id='text'>Pancetta</p><br />
-                 </div> <br />
-
+                    <input type="radio" id="smoked" onChange={() => this.setState((prevState, props) => {return{smoked:prevState.smoked+1}})} name="smoked" value="smoked" class="all_options" /> <label for="smoked" class="option_label">Smoked</label>
+                    <input type="radio" id="turkey" onChange={() => this.setState((prevState, props) => {return{turkey:prevState.turkey+1}})} name="turkey" value="turkey" class="all_options" /> <label for="turkey" class="option_label">Turkey</label>
+                    <input type="radio" id="canadian" onChange={() => this.setState((prevState, props) => { return { canadian: prevState.canadian+1}})} name="canadian" value="canadian" class="all_options" /> <label for="canadian" class="option_label">Candadian</label>
+                    <input type="radio"  id="pancetta" onChange={() => this.setState((prevState, props) => { return { pancetta: prevState.pancetta+1}})} name="pancetta" value="pancetta" class="all_options" /> <label for="pancetta" class="option_label">Pancetta</label>
+                </div>
+                <div>
+                    <p>{this.state.smoked}</p>
+                    <p>{this.state.turkey}</p>
+                    <p>{this.state.canadian}</p>
+                    <p>{this.state.pancetta}</p>
+                </div>
                 <Link to="/orderConformation"><input type="submit" id="button" value="Add item to order" /></Link>
 
                 </form>
