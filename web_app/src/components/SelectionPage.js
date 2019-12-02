0ffa4729c9937aad9def8037b0b15cf49cb0c922
justinbyers <byersjustin3@gmail.com>
2019-12-01 20:31:09 -0500

    Fixed ID transfer

--- web_app/src/components/SelectionPage.js
+++ web_app/src/components/SelectionPage.js
@@ -1,43 +1,3 @@
-// import React, { Component } from 'react';
-// import { Link } from "react-router-dom";
-// import "../styles/selectionStyle.css";
-// import "../styles/commonStyle.css"
-
-// class SelectionPage extends Component {
-//   render() {
-//     return (
-//       <body>
-//         <div className="selectionPage" id="container">
-//           <img id="login_bg" />
-//           <h1 id="head">Make a Selection</h1>
-
-//           <input type='hidden' value='' name='studentID' id='studentID' />
-//           <div class='order'>
-//             <div id='t1'>
-
-//               <figure id='breakfast'>
-//                 <figcaption id="text">BREAKFAST
-//                         </figcaption>
-//                 <img id="bf" />
-//               </figure>
-//             </div>
-//             <div id='t2'>
-
-//               <figure id='pasta'>
-//                 <figcaption >PASTA</figcaption>
-//                 <img id="p" />
-
-//               </figure>
-//             </div>
-//           </div>
-//         </div>
-//       </body>
-//     );
-//   }
-// }
-
-// export default SelectionPage;
-
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import "../styles/breakfastStyle.css";
@@ -48,20 +8,30 @@ var urlData;
 
 class SelectionPage extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      urlDataState: '',
+    };
+  }
+
   componentDidMount(){
     urlData = window.location.href; //localhost:3000/selection?id=012345
 
     urlData = urlData.split('?'); 
     urlData = urlData[1]; //12345
     
-    //urdData= urlData.replace('id=', '');
+    this.setState((prevState, props) => { return { urlDataState: urlData } });
+  }
 
+  updateStates(){
+    this.setState((prevState, props) => { return { urlDataState: urlData } });
   }
 
   render() {
     return (
-      <body>
-        <div className="SelectionPage" id="container">
+      <body onMouseDown={() => this.updateStates()}>
+        <div className="SelectionPage" id="container" >
           <img id="login_bg" />
 
           <h1 id="head">Menu</h1>
@@ -70,14 +40,14 @@ class SelectionPage extends Component {
           <form action="review_order.html" method="GET" name="MenuItem" class="order">
             <div class='grid-container'>
               <div class='grid-item'>
-                <Link to={"/breakfast" + "?" + urlData}>
+                <Link to={"/breakfast" + "?" + this.state.urlDataState}>
                   <input type="radio" id="breakfast" name="BFItem" value="breakfast" class="radio-tools" />
                 </Link>
                 <label id='l_breakfast' for="breakfast" class="item_label"></label>
                 <p id='text'>Breakfast</p>
               </div>
               <div class='grid-item'>
-                <Link to={"/pasta" + "?" + urlData}>
+                <Link to={"/pasta" + "?" + this.state.urlDataState}>
                   <input type="radio" id="pasta" name="PItem" value="pasta" class="radio-tools" />
                 </Link>
                 <label id='l_pasta' for="pasta" class="item_label"></label>
@@ -86,7 +56,6 @@ class SelectionPage extends Component {
             </div>
 
           </form>
-
         </div>
       </body>
 
