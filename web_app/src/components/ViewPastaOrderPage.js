8113bae762d908c8bffb3f81681cc6a6af3d0ed5
mfranz1 <55514493+mfranz1@users.noreply.github.com>
2019-12-03 12:59:11 -0500

    Update ViewPastaOrderPage.js

--- web_app/src/components/ViewPastaOrderPage.js
+++ web_app/src/components/ViewPastaOrderPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import "../styles/viewOrderStyle.css";
-import "../styles/commonStyle.css"
+import "../styles/commonStyle.css";
 
 class ViewPastaOrderPage extends Component {
   render() {
@@ -28,16 +28,110 @@ class Table extends Component {
             <th>Pasta type</th>
             <th>Sauce</th>
             <th>Ingredients</th>
-            <th>Status</th>
+            <th>Order Status</th>
           </tr>
-          <Row />
+          <tr>
+            <td>1</td>
+            <td>635565</td>
+            <td>Penne</td>
+            <td>Pesto</td>
+            <td>
+              <li>Chicken</li>
+              <li>Onion</li>
+              <li>Broccoli</li>
+              <li>Mushrooms</li>
+              <li>Salt/Pepper</li>
+              <li>Garlic</li>
+            </td>
+            <td>
+              <label class="switch">
+                <input type="checkbox"></input>
+                <span class="slider"></span>
+              </label>
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>621448</td>
+            <td>Bowtie</td>
+            <td>Marinara</td>
+            <td>
+              <li>Meatballs</li>
+              <li>Salt/Pepper</li>
+              <li>Garlic</li>
+            </td>
+            <td>
+              <label class="switch">
+                <input type="checkbox"></input>
+                <span class="slider"></span>
+              </label>
+            </td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>729188</td>
+            <td>Bowtie</td>
+            <td>Alfredo</td>
+            <td>
+              <li>Chicken</li>
+              <li>Onion</li>
+              <li>Corn</li>
+              <li>Salt/Pepper</li>
+              <li>OldBay</li>
+            </td>
+            <td>
+              <label class="switch">
+                <input type="checkbox"></input>
+                <span class="slider"></span>
+              </label>
+            </td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>663596</td>
+            <td>Penne</td>
+            <td><li>Pesto</li><li>Alfredo</li></td>
+            <td>
+              <li>Shrimp</li>
+              <li>Corn</li>
+              <li>Salt/Pepper</li>
+              <li>Garlic</li>
+              <li>Italian</li>
+            </td>
+            <td>
+              <label class="switch">
+                <input type="checkbox"></input>
+                <span class="slider"></span>
+              </label>
+            </td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>635111</td>
+            <td>Penne</td>
+            <td>Marinara</td>
+            <td>
+              <li>Meatballs</li>
+              <li>Sausage</li>
+              <li>Onion</li>
+              <li>Salt/Pepper</li>
+              <li>Garlic</li>
+            </td>
+            <td>
+              <label class="switch">
+                <input type="checkbox"></input>
+                <span class="slider"></span>
+              </label>
+            </td>
+          </tr>
         </table>
+        <br/><br/>
       </div>
     );
   }
 }
 
-class Row extends Component {
+/*class Row extends Component {
   render() {
     return (
       <tr>
@@ -50,7 +144,7 @@ class Row extends Component {
       </tr>
     );
   }
-}
+}*/
 
 
 export default ViewPastaOrderPage;
